24a49b0a94848018f8ed3e54300e6d4664c0dc7d
dakshya-rajat <70906749+dakshya-rajat@users.noreply.github.com>
2020-11-05 11:37:32 +0545

    modified contents for release

--- src/sections/joinUs/joinus.js
+++ src/sections/joinUs/joinus.js
@@ -17,7 +17,7 @@ export default ({ data, mobile }) => {
       <Box
         background="p1-l"
         overflow="hidden"
-        width={mobile ? "100%" : "40%"}
+        width="40%"
         pad={{ top: "medium" }}
       >
         <Image
@@ -34,8 +34,13 @@ export default ({ data, mobile }) => {
         <Text code="subtext-r" size="small" color="b2">
           {data.desc}
         </Text>
-        <Box width={mobile ? "100%" : "xsmall"}>
-          <Button secondary label="Apply" onClick={() => setApply(true)} />
+        <Box width={mobile ? "100%" : "small"}>
+          <Button
+            secondary
+            label="Apply"
+            alignSelf="start"
+            onClick={() => setApply(true)}
+          />
         </Box>
       </Box>
       {apply && <ApplyConsultant setApply={setApply} />}
